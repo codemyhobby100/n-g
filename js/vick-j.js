91b48aa163e16b16b4bf1f8e3910dab7472d2954
Codemyhobby <alhassandamilola16@gmail.com>
2024-06-18 17:37:26 +0100

    first commit

---
+++ js/vick-j.js
@@ -0,0 +1,32 @@
+//Scroll reveal//
+
+window.sr = ScrollReveal();
+
+
+sr.reveal('.animate-left',{
+	origin: 'left',
+	duration: 1000,
+	distance: '20rem',
+	delay:200
+});
+
+sr.reveal('.animate-right',{
+	origin: 'right',
+	duration: 1000,
+	distance: '20rem',
+	delay: 300
+});
+
+sr.reveal('.animate-top',{
+	origin: 'top',
+	duration: 1000,
+	distance: '20rem',
+	delay: 300
+});
+
+sr.reveal('.animate-bottom',{
+	origin: 'bottom',
+	duration: 1000,
+	distance: '20rem',
+	delay: 300
+});
